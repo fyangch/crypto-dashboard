aeca200d4586eee0675e7b6af13dd5fad49745ee
Felix Yang <79040988+fyangch@users.noreply.github.com>
2023-01-11 19:29:00 +0100

    Compute percentage gains from recent lows

--- src/market_data.py
+++ src/market_data.py
@@ -59,6 +59,34 @@ def get_klines(
     return klines, current_price
 
 
+def get_gains(
+    klines: pd.DataFrame, 
+    look_back: List[int] = [4, 16, 96],
+    current_price: Optional[float] = None,
+    ) -> List[float]:
+    """
+    Compute the current percentage gains from the lowest lows in the past klines.
+
+    Args:
+        klines
+            Data frame containing the most recent klines.
+        loock_back
+            List containing the number of most recent klines from which the gains will be computed.
+        current_price
+            Optional current price in case the last kline was removed after the retrieval. 
+            If None, the close price of the last data frame row will be used.
+    
+    Returns:
+        List of percentage gains for each look-back value.
+    """
+    lows = [klines['low'].iloc[-offset:].min() for offset in look_back]
+
+    if not current_price:
+        current_price = klines['close'].iloc[-1]
+
+    return [(current_price / low) - 1. for low in lows]
+
+
 def _get_binance_klines(symbol: str, interval: int, num_klines: int):
     """ 
     Fetch klines from Binance. 
