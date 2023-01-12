25118de00d452e3e2d8bdb0f0fb087779b626817
Felix Yang <79040988+fyangch@users.noreply.github.com>
2023-01-12 10:48:39 +0100

    Use dataframe as input for market scan

--- src/pump_screener.py
+++ src/pump_screener.py
@@ -1,30 +1,32 @@
 import pandas as pd
 
 from src.market_data import get_klines, get_gains
+from src.decorators import parallel
 
 
+@parallel(num_processes=3)
 def scan_markets(
-    coins: dict,
+    info_df: pd.DataFrame,
     ) -> pd.DataFrame:
     """
     Scan crypto markets for pumps.
 
     Args:
-        coins
-            Dictionary with information about the coins to consider.
+        info_df
+            Data frame with information about the coins to consider.
     
     Returns:
         Data frame containing the name of the coin and the corresponding gains. 
     """
-    name = []
-    gain_1h = []
-    gain_4h = []
-    gain_1d = []
+    names = info_df.index.values
+    gains_1h = []
+    gains_4h = []
+    gains_1d = []
     
-    for coin in coins:
+    for name in names:
         klines, current_price = get_klines(
-            symbol=coins[coin]["symbol"],
-            exchange=coins[coin]["exchange"],
+            symbol=info_df.loc[name, "symbol"],
+            exchange=info_df.loc[name, "exchange"],
             interval=15,
             num_klines=100,
             min_age=8,
@@ -34,14 +36,15 @@ def scan_markets(
             look_back=[4, 16, 96],
             current_price=current_price,
         )
-        name.append(coin)
-        gain_1h.append(gains[0])
-        gain_4h.append(gains[1])
-        gain_1d.append(gains[2])
+        gains_1h.append(gains[0])
+        gains_4h.append(gains[1])
+        gains_1d.append(gains[2])
     
-    return pd.DataFrame(data={
-        "name": name,
-        "gain_1h": gain_1h,
-        "gain_4h": gain_4h,
-        "gain_1d": gain_1d,
-    })
+    return pd.DataFrame(
+        index=names,
+        data={
+            "gain_1h": gains_1h,
+            "gain_4h": gains_4h,
+            "gain_1d": gains_1d,
+        }
+    )
